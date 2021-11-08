e0b54bb702e86930a9a106857d6bbe07a86936a2
Belinda <35804654+BelindaWWR@users.noreply.github.com>
2021-11-08 15:49:17 +0800

    Update ExternalGTest.cmake
    Change "https" to "git"

--- cmake/ExternalGTest.cmake
+++ cmake/ExternalGTest.cmake
@@ -3,7 +3,7 @@
 
 FetchContent_Declare(
     googletest
-    GIT_REPOSITORY https://github.com/google/googletest.git
+    GIT_REPOSITORY git://github.com/google/googletest.git
     GIT_TAG        703bd9caab50b139428cea1aaff9974ebee5742e # 1.10.0
 )
 FetchContent_GetProperties(googletest)
